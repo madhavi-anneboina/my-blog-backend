54a20e0ea6e6fdc0136091fab66307f7a229935c
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2024-07-11 22:12:46 -0400

    added server

--- src/server.js
+++ src/server.js
@@ -1,11 +1,41 @@
-import express from 'express';
+import express from "express";
 
-const app = express()
+let articlesInfo = [
+  {
+    name: "learn-react",
+    upvotes: 0,
+  },
+  {
+    name: "learn-node",
+    upvotes: 0,
+  },
+  {
+    name: "mongodb",
+    upvotes: 0,
+  }
+];
 
-app.get('/hello',(req,res) =>{
-    res.send("Hello! how are you")
-})
+const app = express();
+app.use(express.json());
 
-app.listen(8000, ()=>{
-    console.log("server is listening on port 8000")
-})
+// app.post('/hello',(req,res) =>{
+//     res.send(`Hello! how are you ${req.body.name}`)
+// })
+
+// app.get('/hello/:name', (req,res) =>{
+//     const {name} = req.params
+//     res.send(`hello ${name}`)
+// })
+
+app.put("/api/articles/:name/upvote", (req, res) => {
+    const {name} = req.params;
+    const article = articlesInfo.find(a => a.name === name)
+    if(article){
+        article.upvotes += 1
+     
+    }
+});
+
+app.listen(8000, () => {
+  console.log("server is listening on port 8000");
+});
